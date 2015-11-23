ba8660d8c40d54b40fa0d8f11707f6ebeb1bb61f
Alex Lee <alexwlee.web@gmail.com>
2015-11-23 22:34:46 +1100

    Complete working version of tictactoe

--- js/gui.js
+++ js/gui.js
@@ -2,19 +2,48 @@ var resetBoard = function(i, item) {
 	$(item).html("");
 }
 
+var playerName = function(name) {
+	if (name === "player1") {
+		return "Player 1";
+	}
+	else {
+		return "Player 2";
+	}
+}
+
 $(document).ready(function() {
 
+	$("#turn").html(playerName(ticTacToe.playerTurn) + "'s turn");
+
 	$(".tile").on("click", function() {
-		var row = $(this).parent().attr("id");
-		var col = $(this).attr("id");
-		ticTacToe.placePiece(row, col);
-		$(this).html(ticTacToe.players[ticTacToe.playerTurn]);
-		ticTacToe.play();
+		if ($(this).html() === "") {
+			var row = $(this).parent().attr("id");
+			var col = parseInt($(this).attr("id"));
+
+			$(this).html(ticTacToe.players[ticTacToe.playerTurn]);
+			ticTacToe.placePiece(row, col);
+
+			if (ticTacToe.checkWin(ticTacToe.playerTurn, row, col)) {
+				var winMessage = $("<h1/>");
+				winMessage.attr("id", "winMessage");
+				winMessage.html(playerName(ticTacToe.playerTurn) + " wins!");
+				$("#board").children().hide();
+				$("#board").append(winMessage);
+			}
+
+			else {
+				ticTacToe.play();
+				$("#turn").html(playerName(ticTacToe.playerTurn) + "'s turn");
+			}
+		}
+
 	});
 
 	$("#reset").on("click", function() {
 		ticTacToe.clearBoard();
-
+		$("#winMessage").remove();
+		$("#turn").html(playerName(ticTacToe.playerTurn) + "'s turn");
 		$(".tile").each(resetBoard);
+		$("#board").children().show();
 	});
 });
--- js/script.js
+++ js/script.js
@@ -20,6 +20,9 @@ var ticTacToe = {
 				this.board[row][i] = "-";
 			}
 		}
+
+		this.playerTurn = "player1";
+		this.turn = false;
 	},
 
 	placePiece: function(row, col) {
@@ -37,5 +40,49 @@ var ticTacToe = {
 			this.turn = false;
 			this.playerTurn = "player1";
 		}
+	},
+
+	checkRow: function(player, row) {
+		var count = 0;
+
+		for (var i = 0; i < this.board[row].length; i++) {
+			if (this.board[row][i] === player) {
+				count++;
+			}
+		}
+
+		return count === 3;
+	},
+
+	checkColumn: function(player, row, col) {
+		var count = 0;
+
+		for (var row in this.board) {
+			if (this.board[row][col] === player) {
+				count++;
+			}
+		}
+
+		return count === 3;
+	},
+
+	checkDiagonals: function(player) {
+		var count = 0;
+
+		if ( 
+			(this.board["row0"][0] === player && this.board["row1"][1] === player && this.board["row2"][2] === player) ||
+			(this.board["row0"][2] === player && this.board["row1"][1] === player && this.board["row2"][0] === player) 
+			) {
+			return true;
+		}
+	},
+
+	checkWin: function(player, row, col) {
+		var player = this.players[player];
+
+		if (this.checkRow(player, row) || this.checkColumn(player, row, col) || this.checkDiagonals(player) ) {
+			return true;
+		}
+		return false;
 	}
 }
